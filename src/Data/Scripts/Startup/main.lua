6a6880fbf5809182eee453718052f61295001750
Rizart Dokollari <r.dokollari@gmail.com>
2025-02-19 02:17:30 +0000

    fix: Dynamically enable buffs.

--- src/Data/Scripts/Startup/main.lua
+++ src/Data/Scripts/Startup/main.lua
@@ -1,51 +1,38 @@
 adaptive_hud = {
-    execution_interval_ms = "1000",
+    stats_execution_interval_ms = "2000",
     health_threshold = "0.75",
     stamina_threshold = "100",
     stats_timer_id = nil,
     menu_close_timer_id = nil,
     force_show_stats = false,
+    always_hide_stats = false,
+    always_hide_compass = true,
+    always_hide_food = true,
+    always_hide_weapon = true,
+    first_on_action_executed = true,
     onload_executed = false,
-    first_on_action_executed = true
+    menu_close_timer = 7000
 }
 
-local log_prefix = "$5[AdaptiveHUD] "
+local log_prefix = "adaptive_hud"
 
-local function log_info(message, ...)
-    local formatted_message = string.format(log_prefix .. message, ...)
+local function log_error(message, ...)
+    local formatted_message = string.format("$4[%s.ERROR] " .. message, log_prefix, ...)
     System.LogAlways(formatted_message)
 end
 
-function adaptive_hud:OnLoad()
+local function log_info(message, ...)
+    local formatted_message = string.format("$5[%s.INFO] " .. message, log_prefix, ...)
+    System.LogAlways(formatted_message)
+end
+
+log_info("Mod loaded.")
+
+function adaptive_hud:StatsTimerOnLoad()
     if self.onload_executed then return end
-    log_info("OnLoad.init")
+    log_info("OnLoad")
     self.onload_executed = true
-    self:start_stats_timer(tonumber(self.execution_interval_ms))
-end
-
-function adaptive_hud:start_stats_timer(interval_ms)
-    log_info("start_stats_timer with interval %d ms", interval_ms)
-    if self.stats_timer_id then Script.KillTimer(self.stats_timer_id) end
-
-    log_info("start_stats_timer setting timer")
-    self.stats_timer_id = Script.SetTimer(interval_ms, function(nTimerId)
-        log_info("start_stats_timer callback.")
-        self:updateStats()
-    end)
-end
-
-function adaptive_hud:start_menu_close_timer()
-    if self.menu_close_timer_id then Script.KillTimer(self.menu_close_timer_id) end
-    self.menu_close_timer_id = Script.SetTimer(7000, function(nTimerId)
-        self.force_show_stats = false
-        local showStats = self:has_low_stats() and 1 or 0
-        System.SetCVar('wh_ui_ShowStats', showStats)
-        System.SetCVar('wh_ui_showCompass', 0)
-        System.SetCVar('wh_ui_ShowQAMFood', 0)
-        System.SetCVar('wh_ui_ShowQAMWeapon', 0)
-        System.SetCVar('wh_ui_ShowBuffs', 0)
-        self.menu_close_timer_id = nil
-    end)
+    self:start_stats_timer(tonumber(self.stats_execution_interval_ms))
 end
 
 function adaptive_hud:has_low_stats()
@@ -61,33 +48,79 @@ function adaptive_hud:has_low_stats()
     return health_low or stamina_low
 end
 
-function Player:OnLoad(saved)
-    log_info("Player:OnLoad")
-	BasicActor.OnLoad(self, saved);
-    log_info("Player:OnLoadSecond")
-	self.WorldTimePausedReasons = saved.WorldTimePausedReasons;
+function adaptive_hud:get_show_stats()
+    log_info("get_show_stats called")
+    local showStats = self.always_hide_stats and "0" or (self:has_low_stats() and "1" or "0")
+
+    log_info(string.format("Calculated showStats: %s", showStats))
+    return showStats
 end
 
 function adaptive_hud:updateStats()
     log_info("updateStats called")
-
     if self.force_show_stats then
-        log_info("updateStats.starting new timer")
-        self:start_stats_timer(tonumber(self.execution_interval_ms))
+        log_info("updateStats - force show stats enabled")
+        self:start_stats_timer(tonumber(self.stats_execution_interval_ms))
         return
     end
-
-    local showStats = self:has_low_stats() and "1" or "0"
-    log_info("updateStats.showStats: %s", showStats)
-    System.SetCVar('wh_ui_ShowStats', showStats)
-
-    log_info("updateStats.starting new timer")
-    self:start_stats_timer(tonumber(self.execution_interval_ms))
+    System.SetCVar('wh_ui_ShowStats', self:get_show_stats())
+    log_info("updateStats completed, restarting timer")
+    self:start_stats_timer(tonumber(self.stats_execution_interval_ms))
 end
 
+function adaptive_hud:start_stats_timer(interval_ms)
+    log_info("start_stats_timer")
+    if self.stats_timer_id then Script.KillTimer(self.stats_timer_id) end
+    self.stats_timer_id = Script.SetTimer(interval_ms, function(nTimerId)
+        log_info("start_stats_timer.callback")
+        self:updateStats()
+    end)
+end
+
+function adaptive_hud:start_menu_close_timer()
+    log_info("start_menu_close_timer called")
+    if self.menu_close_timer_id then
+        Script.KillTimer(self.menu_close_timer_id)
+    end
+    self.menu_close_timer_id = Script.SetTimer(adaptive_hud.menu_close_timer, function(nTimerId)
+        log_info("start_menu_close_timer.callback")
+        self.force_show_stats = false
+        System.SetCVar('wh_ui_ShowStats', self:get_show_stats())
+        System.SetCVar('wh_ui_ShowStats', "0")
+        System.SetCVar('wh_ui_showCompass', "0")
+        System.SetCVar('wh_ui_ShowQAMFood', "0")
+        System.SetCVar('wh_ui_ShowQAMWeapon', "0")
+        System.SetCVar('wh_ui_ShowBuffs', "0")
+        self.menu_close_timer_id = nil
+    end)
+end
+
+local original_OnInit = Player.OnInit
+
+function Player:OnInit()
+    log_info("Player:OnInit")
+
+    if original_OnInit then
+        local success, result = pcall(original_OnInit, self)
+        if not success then
+            log_error("Error in original Player:OnInit: " .. tostring(result))
+        end
+    end
+
+    adaptive_hud:StatsTimerOnLoad()
+end
+
+local original_OnAction = Player.OnAction
+
 function Player:OnAction(action, activation, value)
-    adaptive_hud:OnLoad()
-    log_info(string.format("action: %s, activation: %s", action, activation))
+    if original_OnAction then
+        local success, result = pcall(original_OnAction, self, action, activation, value)
+        if not success then
+            log_error("Error in original Player:OnAction: " .. tostring(result))
+        end
+    end
+
+    adaptive_hud:StatsTimerOnLoad()
 
     local valid_actions = {
         open_apse_map_keyboard = true,
@@ -97,19 +130,17 @@ function Player:OnAction(action, activation, value)
         open_apse_inventory_keyboard = true,
         open_apse_codex = true
     }
-
-    if adaptive_hud.first_on_action_executed then 
+    if adaptive_hud.first_on_action_executed then
         adaptive_hud.first_on_action_executed = false
+        log_info("Player:OnAction first_on_action_executed")
     elseif not valid_actions[action] or activation ~= "release" then
-       return 
+        return
     end
-
-    log_info("Showing hud")
-    System.SetCVar('wh_ui_ShowStats', 1)
-    System.SetCVar('wh_ui_showCompass', 1)
-    System.SetCVar('wh_ui_ShowQAMFood', 1)
-    System.SetCVar('wh_ui_ShowQAMWeapon', 1)
-    System.SetCVar('wh_ui_ShowBuffs', 1)
+    System.SetCVar('wh_ui_ShowStats', adaptive_hud.always_hide_stats and "0" or "1")
+    System.SetCVar('wh_ui_showCompass', adaptive_hud.always_hide_compass and "0" or "1")
+    System.SetCVar('wh_ui_ShowQAMFood', adaptive_hud.always_hide_food and "0" or "1")
+    System.SetCVar('wh_ui_ShowQAMWeapon', adaptive_hud.always_hide_weapon and "0" or "1")
+    System.SetCVar('wh_ui_ShowBuffs', "1")
     adaptive_hud.force_show_stats = true
     adaptive_hud:start_menu_close_timer()
 end
